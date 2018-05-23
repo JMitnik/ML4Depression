903c4e8a43b8f82e7f68857734558ef2904eb1e6
Jonathan Mitnik <JMitnik@gmail.com>
2018-05-23 16:18:02 +0200

    Including previous days

--- feature_engineering.py
+++ feature_engineering.py
@@ -25,7 +25,7 @@ pd_sample_patient.index = pd_sample_patient['xEmaDate']
 
 # Get representation of each of the moods of the patient
 pd_sample_patient_moods = pd.get_dummies(
-    pd_sample_patient['xEmaQuestion'], prefix='mood_question')
+    pd_sample_patient['xEmaQuestion'], prefix='ema_question')
 pd_sample_patient = pd_sample_patient.join(pd_sample_patient_moods)
 
 patient_moods_index = pd_sample_patient_moods.multiply(
@@ -40,7 +40,7 @@ pd_sample_patient = pd_sample_patient[pd_sample_patient['xEmaSchedule'] != 4]
 
 sliding_window = 7
 
-mood_question_columns = pd_sample_patient.filter(regex="mood_question_\d")
+mood_question_columns = pd_sample_patient.filter(regex="ema_question_\d")
 pd_resampled_days = pd_sample_patient.resample('1d')
 
 patient_base_features = pd.DataFrame(index=pd_resampled_days.index)
@@ -52,29 +52,36 @@ for col in mood_question_columns:
 # Getting a number of generic statistics for the different features
 patient_ml_features = pd.DataFrame(index=patient_base_features.index)
 
-for col in patient_base_features.fillna(0):
-    patient_ml_features['avg_'+col+'_'+sliding_window+'_days'] = patient_base_features[col].rolling(
-        str(sliding_window)+'d').mean().shift(1)
-    patient_ml_features['min_'+col+'_'+sliding_window+'_days'] = patient_base_features[col].rolling(
-        str(sliding_window)+'d').min().shift(1)
-    patient_ml_features['max_'+col+'_'+sliding_window+'_days'] = patient_base_features[col].rolling(
-        str(sliding_window)+'d').max().shift(1)
-    patient_ml_features['std_'+col+'_'+sliding_window+'_days'] = patient_base_features[col].rolling(
-        str(sliding_window)+'d').std().shift(1)
-
-patient_ml_features = patient_ml_features.fillna(0)
-
-#TODO: Get a better representation than this.
+#TODO: Get a better representation than this
 patient_q_asked = pd_sample_patient['xEmaSchedule'].resample('1d').count()
 patient_q_asked[:7] = 10
 patient_q_asked[len(patient_q_asked) - 7:] = 10
 patient_q_asked[7:len(patient_q_asked) - 7][patient_q_asked > 1] = 10
 patient_q_asked[patient_q_asked <= 1] = 1
 
-pd_engagement = pd_sample_patient['xEmaSchedule'].resample('1d') / q_asked
-pd_engagement = pd_engagement.fillna(0)
-pd_engagement.fillna(0)
+pd_engagement = pd_sample_patient['xEmaSchedule'].resample(
+    '1d') / patient_q_asked
+pd_engagement =  pd_engagement.fillna(0)
+
+#%%
+patient_base_features = patient_base_features.join(pd_engagement).rename(columns=({'xEmaSchedule': 'actual_engagement'}))
+patient_base_features
+#%%
+for col in patient_base_features.fillna(0):
+    patient_ml_features['avg_'+col+'_'+str(sliding_window)+'_days'] = patient_base_features[col].rolling(
+        str(sliding_window)+'d').mean().shift(1)
+    patient_ml_features['min_'+col+'_'+str(sliding_window)+'_days'] = patient_base_features[col].rolling(
+        str(sliding_window)+'d').min().shift(1)
+    patient_ml_features['max_'+col+'_'+str(sliding_window)+'_days'] = patient_base_features[col].rolling(
+        str(sliding_window)+'d').max().shift(1)
+    patient_ml_features['std_'+col+'_'+str(sliding_window)+'_days'] = patient_base_features[col].rolling(
+        str(sliding_window)+'d').std().shift(1)
+
+patient_ml_features = patient_ml_features.fillna(0)
 
 # This is eventually what we export
 patient_x = patient_ml_features
 patient_y = pd_engagement
+
+#%%
+patient_ml_features
