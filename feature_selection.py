b0bbe0c86482eb36d68e8b5391ff65249a09eed1
Jonathan Mitnik <JMitnik@gmail.com>
2018-06-21 15:53:53 +0200

    Untested Correlation and Forward Selection

--- feature_selection.py
+++ feature_selection.py
@@ -1,7 +1,9 @@
 import math
 import copy
+from scipy.stats import pearsonr
 from predicting import make_algorithms
 from helpers import get_relevant_dates, convert_features_to_statistics, split_dataset, get_patient_id_by_rank
+from operator import itemgetter
 
 BE_EMA_COLS = [
     'avg_count_ema_q_2_7_days',
@@ -53,74 +55,52 @@ def backward_selection(max_features, ml_models, patient_x, patient_y):
         features.remove(worst_feature)
     return features
 
-# TODO: Create forwards feature selection.
-# # Specifies feature selection approaches for classification to identify the most important features.
-# class FeatureSelectionRegression:
+# TODO: Needs to be tested
+def forward_selection(max_features, ml_models, patient_x, patient_y):
+    ordered_features = []
+    ordered_scores = []
+    features = []
 
-#     # Forward selection for classification which selects a pre-defined number of features (max_features)
-#     # that show the best accuracy. We assume a decision tree learning for this purpose, but
-#     # this can easily be changed. It return the best features.
-#     def forward_selection(self, max_features, X_train, y_train):
-#         ordered_features = []
-#         ordered_scores = []
+    prev_best_perf = math.inf
 
-#         # Start with no features.
-#         selected_features = []
-#         ra = RegressionAlgorithms()
-#         re = RegressionEvaluation()
-#         prev_best_perf = sys.float_info.max
+    for i in range(0, max_features):
+        features_left = list(set(patient_x.columns) - set(features))
+        best_performance = math.inf
+        best_feature = ''
 
-#         # Select the appropriate number of features.
-#         for i in range(0, max_features):
+        for feature in features_left:
+            sample_models = copy.deepcopy(ml_models)
+            temp_features = copy.deepcopy(features)
+            temp_features.append(feature)
+            fet_patient_x = patient_x[temp_features]
+            models = make_algorithms(sample_models)
+            performance = models[0]['score']
+            performace = next(item for item in performance if item.get('mae'))['mae']
 
-#             #Determine the features left to select.
-#             features_left = list(set(X_train.columns) - set(selected_features))
-#             best_perf = sys.float_info.max
-#             best_feature = ''
+            if performance < best_performance:
+                best_performance = performance
+                best_feature = feature
 
-#             # For all features we can still select...
-#             for f in features_left:
-#                 temp_selected_features = copy.deepcopy(selected_features)
-#                 temp_selected_features.append(f)
+        features.append(best_feature)
+        prev_best_perf = best_performance
 
-#                 # Determine the mse of a decision tree learner if we were to add
-#                 # the feature.
-#                 pred_y_train, pred_y_test = ra.decision_tree(X_train[temp_selected_features], y_train, X_train[temp_selected_features])
-#                 perf = re.mean_squared_error(y_train, pred_y_train)
+    return features
 
-#                 # If the performance is better than what we have seen so far (we aim for low mse)
-#                 # we set the current feature to the best feature and the same for the best performance.
-#                 if perf < best_perf:
-#                     best_perf = perf
-#                     best_feature = f
-#             # We select the feature with the best performance.
-#             selected_features.append(best_feature)
-#             prev_best_perf = best_perf
-#             ordered_features.append(best_feature)
-#             ordered_scores.append(best_perf)
-#         return selected_features, ordered_features, ordered_scores
+# TODO: Needs to be tested
+def correlate_features(max_features, ml_models, patient_x, patient_y):
+    correlations = []
+    full_columns_and_corr = []
+    abs_columns_and_corr = []
 
-#     # Backward selection for classification which selects a pre-defined number of features (max_features)
-#     # that show the best accuracy. We assume a decision tree learning for this purpose, but
-#     # this can easily be changed. It return the best features.
+    for i in range(0, len(patient_x.columns)):
+        corr_score, p = pearsonr(patient_x.columns[i], patient_y)
+        correlations.append(abs(corr_score))
 
-#     # Select features based upon the correlation through the Pearson coefficient.
-#     # It return the max_features best features.
-#     def pearson_selection(self, max_features, X_train, y_train):
-#         correlations = []
-#         full_columns_and_corr = []
-#         abs_columns_and_corr = []
+        if np.isfinite(corr_scores):
+            full_columns_and_corr.append((patient_x.columns[i], corr_score))
+            abs_columns_and_corr.append((patient_x.columns[i], abs(corr_score)))
 
-#         # Compute the absolute correlations per column.
-#         for i in range(0, len(X_train.columns)):
-#             corr, p = pearsonr(X_train[X_train.columns[i]], y_train)
-#             correlations.append(abs(corr))
-#             if np.isfinite(corr):
-#                 full_columns_and_corr.append((X_train.columns[i], corr))
-#                 abs_columns_and_corr.append((X_train.columns[i], abs(corr)))
+    sorted_attributes = sorted(abs_columns_and_corr, key=itemgetter(1), reverse=True)
+    res_list = [x[0] for x in sorted_attributes[:max_features]]
 
-#         sorted_attributes = sorted(abs_columns_and_corr,key=itemgetter(1), reverse=True)
-#         res_list = [x[0] for x in sorted_attributes[0:max_features]]
-
-#         # And return the most correlated ones.
-#         return res_list, sorted(full_columns_and_corr,key=itemgetter(1), reverse=True)
+    return res_list, sorted(full_columns_and_corr, key=itemgetter(1), reverse=True)
